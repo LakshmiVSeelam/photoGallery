4a1253d5477d30da28b8474c5285b2f019b9416e
Lakshmi S <lakshmi.s@gramener.org>
2020-10-09 23:20:25 +0530

    testing

---
+++ common.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    $.getJSON('https://gist.githubusercontent.com/LakshmiVSeelam/dbf7c3e019b5b84ba68b2f8bd3f5bbb9/raw/f3304d4527297b9bbeddf62553b0ba4f9d0b9f0b/UP_geojson.json', function (data) {
+        debugger
+    })
+})
